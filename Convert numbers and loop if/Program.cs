bcc21733d64fbea7ade17be2032325de519042fc
domic1 <115622468+domic1@users.noreply.github.com>
2024-01-31 20:48:27 +0100

    Dodaj pliki projektów.

---
+++ Convert numbers and loop if/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Done_is_better_than_good
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1;
+            double number2;
+            Console.WriteLine("Podaj pierwszą cyfrę");
+            double.TryParse(Console.ReadLine(), out number1);
+            Console.WriteLine("Podaj drugą cyfrę");
+            double.TryParse(Console.ReadLine(), out number2);
+
+            Console.WriteLine("Suma tych liczb jest równa:" + (number1 + number2));
+            Console.WriteLine("Wynik z dzielenia jest równy :" + (number1 / number2));
+        }
+    }
+}
